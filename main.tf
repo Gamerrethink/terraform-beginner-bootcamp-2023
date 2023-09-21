505ce62f5975ab6da246943ed9ae0b20d001ca34
Marquis <marquismb.mb@gmail.com>
2023-09-21 21:18:58 +0000

    minor adjustment

--- main.tf
+++ main.tf
@@ -29,8 +29,8 @@ provider "aws" {
 resource "random_string" "bucket_name" {
   lower = true
   upper = false
-  length           = 32
-  special          = false
+  length = 32
+  special = false
 }
 
 resource "aws_s3_bucket" "example" {
