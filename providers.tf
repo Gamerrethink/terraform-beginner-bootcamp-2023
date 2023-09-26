7659ca718f1afa64bfd85c10601a81a472c6b923
Marquis <marquismb.mb@gmail.com>
2023-09-26 15:18:11 -0400

    #21 change structure of root module and add tagging (#22)

---
+++ providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  cloud {
+    organization = "terraformbootcamp2023"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+provider "aws" {
+  
+}
