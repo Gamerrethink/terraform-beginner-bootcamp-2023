11743bcaddb36aaaa601cef541f5d0b271e6996e
Marquis <marquismb.mb@gmail.com>
2023-09-27 18:21:58 +0000

    #25 refactor S3 Bucket into TerraHouse aws module

--- providers.tf
+++ providers.tf
@@ -6,13 +6,14 @@ terraform {
       name = "terra-house-1"
     }
   }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "5.16.2"
     }
   }
-}
+ } 
 
 provider "random" {
   # Configuration options
